a39e9c3c8ac429c11fe5057b2f541d864ca1dea5
Shihabul-Shuvo <shihabul900@gmail.com>
2026-06-18 22:39:27 +0600

    feat: admin registration for all models

--- Django-React/Assignments/Module 24 Assignment/hospital_api/apps/medicines/admin.py
+++ Django-React/Assignments/Module 24 Assignment/hospital_api/apps/medicines/admin.py
@@ -1,1 +1,10 @@
-# TODO
+from django.contrib import admin
+
+from .models import Medicine
+
+
+@admin.register(Medicine)
+class MedicineAdmin(admin.ModelAdmin):
+    list_display = ("name", "unit", "description")
+    search_fields = ("name",)
+    list_filter = ("unit",)
